acf591ca1849fbe19812b83de6571a5968fb90b3
Siddhesh Chavan <siddhesh8220@gmail.com>
2022-02-24 17:57:26 +0530

    final commit

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,8 @@
-# In vs code ctrl+shift+p > python:select interpreter(for import error)
+# In vs code ctrl+shift+p > python:select interpreter >select env(for import error)
 # Ref: https://www.bezkoder.com/django-rest-api/
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .serializer import TodoSerializer
 from .models import Todo
@@ -15,40 +14,55 @@ def home(request):
 
 @api_view(['Get'])
 def index(request):
-    todos = Todo.objects.all()
-    serializer = TodoSerializer(todos, many=True)
-    return Response(serializer.data)
+    try:
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({"message": str(e)}, status=422)
 
 
 @api_view(['POST'])
 def store(request):
-    updatedData = request.data #req.data gives parsed data
-    generated_id = f"{uuid.uuid4()}"
-    updatedData["id"] = generated_id  
-    serializer = TodoSerializer(data = updatedData) # seriliazes data to model 
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    try:
+        updatedData = request.data #req.data gives parsed data
+        generated_id = f"{uuid.uuid4()}"
+        updatedData["id"] = generated_id  
+        serializer = TodoSerializer(data = updatedData) # seriliazes data to model 
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({"message": str(e)},status=500)
 
 
 @api_view(['GET'])
 def show(request, todoId):
-    todo = Todo.objects.get(id = todoId)
-    serializer = TodoSerializer(todo, many= False)
-    return Response(serializer.data)
+    try:
+        todo = Todo.objects.get(id = todoId)
+        serializer = TodoSerializer(todo, many= False)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({"message": "Todo not found"}, status=404)
 
 
 @api_view(['PATCH'])
 def update(request, todoId):
-    todo = Todo.objects.get(id = todoId)
-    serializer = TodoSerializer(todo, data = request.data, partial = True)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    try:
+        todo = Todo.objects.get(id = todoId)
+        serializer = TodoSerializer(todo, data = request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({"message": "Todo not found"}, status=404)
 
 
 @api_view(['DELETE'])
 def delete(request, todoId):
-    todo = Todo.objects.get(id = todoId)
-    todo.delete()
-    return Response("Item Successfully Deleted")
+    try:
+        todo = Todo.objects.get(id = todoId)
+        todo.delete()
+        return Response({"message": "Item Successfully Deleted"})
+    except Exception as e:
+        return Response({"message": "Todo not found"}, status=404)
